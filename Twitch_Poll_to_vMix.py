b3733294a9e2a065949bd257b6d86942dda75d26
Prospere <prospere.lucel@gmail.com>
2021-06-28 13:23:27 +0200

    Update

--- Twitch_Poll_to_vMix.py
+++ Twitch_Poll_to_vMix.py
@@ -5,15 +5,16 @@ import time
 import PySimpleGUI as sg
 import requests
 import os
+import json
 path = os.getcwd()
+from pythonosc.udp_client import SimpleUDPClient
+ip = "192.168.100.210"
+port = 8000
+client = SimpleUDPClient(ip, port)
 
 poll_finish = False
 thread_stop = False
 current_state = "Waiting..."
-
-def Start_Threads():
-    t01 = threading.Thread(target=GUI)
-    t01.start()
 
 def WebController(url):
     global driver
@@ -22,35 +23,43 @@ def WebController(url):
     global thread_stop
     try:
         options = uc.ChromeOptions()
+        options.add_argument('--headless')
         driver = uc.Chrome(options=options)
-        #options.add_argument('--headless')
         #driver = uc.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe', options=options)
-        driver.get(url)
+        driver.get("https://www.twitch.tv/popout/"+str(poll)+"/poll")
         scan_active = True
     except:
         current_state = "Webdriver Error"
 
 
 def to_vMix(info_list):
-    requests.post("http://localhost:8088/api/?Function=SetText&Input=sondage&SelectedIndex=0&Value="+str(info_list[0]))
 
-    requests.post("http://localhost:8088/api/?Function=SetText&Input=sondage&SelectedIndex=1&Value="+str(info_list[1]))
-    requests.post("http://localhost:8088/api/?Function=SetText&Input=sondage&SelectedIndex=2&Value="+str(info_list[1]))
-    requests.post("http://localhost:8088/api/?Function=SetText&Input=sondage&SelectedIndex=3&Value="+str(info_list[2]))
+    idx_question = "4"
+    idx_vote = ""
+    idx_score1 = "1"
+    idx_score2 = "3"
+    idx_name1 = "0"
+    idx_name2 = "2"
 
-    requests.post("http://localhost:8088/api/?Function=SetText&Input=sondage&SelectedIndex=4&Value="+str(info_list[3]))
-    requests.post("http://localhost:8088/api/?Function=SetText&Input=sondage&SelectedIndex=5&Value="+str(info_list[3]))
-    requests.post("http://localhost:8088/api/?Function=SetText&Input=sondage&SelectedIndex=6&Value="+str(info_list[4]))
+    #return (title_txt, answer_list[0], scores_list[0], answer_list[1], scores_list[1], total_votes)
 
-    requests.post("http://localhost:8088/api/?Function=SetText&Input=sondage&SelectedIndex=7&Value="+str(info_list[5]))
-    requests.post("http://localhost:8088/api/?Function=SetText&Input=sondage&SelectedIndex=8&Value="+str(info_list[5]))
-    requests.post("http://localhost:8088/api/?Function=SetText&Input=sondage&SelectedIndex=9&Value="+str(info_list[6]))
+    txt_question = str(info_list[0])
+    txt_reponse1 = str(info_list[1])
+    txt_score1 = str(info_list[2])
+    txt_reponse2 = str(info_list[3])
+    txt_score2 = str(info_list[4])
+    txt_score1 = txt_score1.replace(" ", "")
+    txt_score2 = txt_score2.replace(" ", "")
 
-    requests.post("http://localhost:8088/api/?Function=SetText&Input=sondage&SelectedIndex=10&Value="+str(info_list[7]))
-    requests.post("http://localhost:8088/api/?Function=SetText&Input=sondage&SelectedIndex=11&Value="+str(info_list[7]))
-    requests.post("http://localhost:8088/api/?Function=SetText&Input=sondage&SelectedIndex=12&Value="+str(info_list[8]))
+    client.send_message("/score1/", txt_score1)
+    client.send_message("/score2/", txt_score2)
+    ##requests.post(vmix_url+"SetText&Input="+vmix_input+"&SelectedIndex="+idx_vote+"&Value="+"VOTES : "+str(info_list[5]))
 
-    requests.post("http://localhost:8088/api/?Function=SetText&Input=sondage&SelectedIndex=13&Value="+str(info_list[9])+str(" VOTES"))
+    requests.post(vmix_url+"SetText&Input="+vmix_input+"&SelectedIndex="+idx_question+"&Value="+txt_question)
+    requests.post(vmix_url+"SetText&Input="+vmix_input+"&SelectedIndex="+idx_score1+"&Value="+txt_score1+"%")
+    requests.post(vmix_url+"SetText&Input="+vmix_input+"&SelectedIndex="+idx_score2+"&Value="+txt_score2+"%")
+    requests.post(vmix_url+"SetText&Input="+vmix_input+"&SelectedIndex="+idx_name1+"&Value="+txt_reponse1)
+    requests.post(vmix_url+"SetText&Input="+vmix_input+"&SelectedIndex="+idx_name2+"&Value="+txt_reponse2)
 
 
 
@@ -63,11 +72,10 @@ def Parse_Info():
     votes = []
     print("Parsing info...")
     try:
-        ##Get Title
-        title = driver.find_elements_by_xpath("//p[contains(@class,\"sc-AxirZ\")]")
-        print (title)
-        for txt in title:
-            title_txt = txt.text
+        ##Get all the text field
+        data_list = driver.find_elements_by_xpath("//p[contains(@class,\"sc-AxirZ\") and not(@class=\"sc-AxirZ ktnnZK\") and not(@class=\"sc-AxirZ epwTZM\") and not(@class=\"sc-AxjAm StDqN\")]")
+        state = data_list[0].text
+        title_txt = data_list[1].text
         ##Get Answer
         answers = driver.find_elements_by_xpath("//p[contains(@data-test-selector,\"title\")]")
         for txt in answers:
@@ -84,17 +92,18 @@ def Parse_Info():
             vote = vote.replace(" ","")
             votes.append(int(vote))
         total_votes = sum(votes)
-
-        current_score = [int(scores_list[0]),int(scores_list[1]),int(scores_list[2]),int(scores_list[3])]
+        current_score = [int(scores_list[0]),int(scores_list[1])]
         print("Title: ", title_txt)
         print("Answer_List: ", answer_list)
         print("Scores_list: ", scores_list)
+        print("Total_Votes: ", total_votes)
 
-        return (title_txt,answer_list[0],scores_list[0],answer_list[1],scores_list[1],answer_list[2],scores_list[2],answer_list[3],scores_list[3],total_votes)
+        return (title_txt,answer_list[0],scores_list[0],answer_list[1],scores_list[1],total_votes)
     except:
         current_state = "Parsing error..."
         print("Parsing error...")
         return
+
 
 def Ranking(current_score):
     print ("Current_Score: ",current_score)
@@ -139,7 +148,7 @@ def Check_Poll():
     try:
         finish = driver.find_element_by_xpath("//p[contains(@data-test-selector,\"header\")]")
         if finish.text == "Sondage terminé":
-            print("Sondage terminé")
+            print(finish.text)
             scan_active = False
             current_state = "Sondage terminé"
             if not poll_finish:
@@ -150,16 +159,16 @@ def Check_Poll():
         scan_active = True
 
 
-def Scan_Loop(url):
+def Scan_Loop(poll):
     global scan_active
     global current_state
     global thread_stop
     global poll_finish
     current_state = "Loading URL..."
-    WebController(url)
-    print("Scanning URL : ", url)
+    WebController(poll)
+    print("Scanning URL : ", poll)
     while True:
-        time.sleep(1)
+        time.sleep(0.25)
         try:
             Check_Poll()
             if scan_active:
@@ -178,25 +187,30 @@ def Scan_Loop(url):
         except:
             current_state = "Error"
 
-def GUI():
-    global thread_stop
-    global current_state
+
+if __name__ == "__main__":
+    poll = "3hitcombo"
+    vmix_ip = "127.0.0.1"
+    vmix_port = "8088"
+    vmix_input = "Sondage_B"
+
     sg.theme('DarkBrown1')
-    image_to_center = [[sg.Image(filename="logo2hdp.png")]]
     #[sg.Column(image_to_center, vertical_alignment='center', justification='center', k='-C-')],
     layout = [
                  [sg.Frame('Status :', layout=[
-                     [
-                         sg.T(current_state, key='_STATE_',size=(18, 1))
-                     ]]),sg.Image(filename="logo2hdp.png")],
-                [sg.Input(key='_URL_POLL_',default_text="https://www.twitch.tv/popout/prospere/poll")],
+                     [sg.T(current_state, key='_STATE_',size=(18, 1))
+                     ]]),sg.Image(filename="Logo_NES.png",tooltip=("nesprod.net / Romain.Desveaux@nesprod.net"), pad=((0, 0),(0,0)))],
+                [sg.Text("vMix IP : ",size=(12, 1)),sg.Input(key='_VMIX_IP_',default_text=vmix_ip,justification="left",size = (15, 1))],
+                [sg.Text("vMix Port : ",size=(12, 1)),sg.Input(key='_VMIX_PORT_',default_text=vmix_port,justification="left",size = (15, 1))],
+                [sg.Text("vMix Input : ",size=(12, 1)),sg.Input(key='_VMIX_INPUT_',default_text=vmix_input,justification="left",size = (15, 1))],
+                [sg.Text("Twitch Channel : ",size=(12, 1)),sg.Input(key='_URL_POLL_',size=(15,1),default_text=poll,)],
                 [sg.Button('Start',key='_START_'),
                 sg.Button('Stop', key='_STOP_',disabled=True),
                 sg.Exit()],
             ]
 
 
-    window = sg.Window('2HDP - Twitch Poll to vMix', layout, keep_on_top=True, auto_size_text=True)
+    window = sg.Window('NES - Twitch Poll to vMix', layout, keep_on_top=True, auto_size_text=True,grab_anywhere = True,finalize=True,element_justification="left")
 
     while True:
         event, values = window.read(timeout=1000)
@@ -208,17 +222,19 @@ def GUI():
         if event == '_START_':
             window['_START_'].update(disabled=True)
             window['_STOP_'].update(disabled=False)
-            url = values['_URL_POLL_']
-            t = threading.Thread(target=Scan_Loop, args=(url,))
+            vmix_ip = values['_VMIX_IP_']
+            vmix_port = values['_VMIX_PORT_']
+            vmix_input = values['_VMIX_INPUT_']
+            vmix_url = "http://"+str(vmix_ip)+":"+str(vmix_port)+"/api/?Function="
+            poll = values['_URL_POLL_']
+            t = threading.Thread(target=Scan_Loop, args=(poll,))
             t.start()
             thread_stop = False
+
         if event == '_STOP_':
             window['_START_'].update(disabled=False)
             window['_STOP_'].update(disabled=True)
             thread_stop = True
 
     window.close()
-
-if __name__ == "__main__":
-    Start_Threads()
 
